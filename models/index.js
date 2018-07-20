c362328c5e1c57ed800c9195c138dbccb02eb7e1
ruben-r <ruben.ruiz@outlook.com>
2018-07-19 21:27:31 -0500

    added user table to db, corrected index.js

--- models/index.js
+++ models/index.js
@@ -1,16 +1,52 @@
+'use strict';
 
-event = {
-    selectAll: function (location, category) {
-        $.ajax({
-            url: `http://api.eventful.com/json/events/search?app_key=${process.env.apikey}&location=${location}&category=${category}`,
-            method: GET
-        }
-            , function (err, data) {
-                if (err) {
-                    throw err;
-                }
-                return data.events
+var fs        = require('fs');
+var path      = require('path');
+var Sequelize = require('sequelize');
+var basename  = path.basename(__filename);
+var env       = process.env.NODE_ENV || 'development';
+var config    = require(__dirname + '/../config/config.json')[env];
+var db        = {};
 
-            })
-    }
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    var model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+var event = {
+  selectAll: function (location, category) {
+      $.ajax({
+          url: `http://api.eventful.com/json/events/search?app_key=${process.env.apikey}&location=${location}&category=${category}`,
+          method: GET
+      }
+          , function (err, data) {
+              if (err) {
+                  throw err;
+              }
+              return data.events
+
+          })
+  }
+}
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
+var db = require("./models");
 
 var PORT = process.env.PORT || 8080;
 
@@ -26,7 +26,8 @@ app.set("view engine", "handlebars");
 // app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
