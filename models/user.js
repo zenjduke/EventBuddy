5e8d302ff54977080c66dba64277130e7b28660b
ruben-r <ruben.ruiz@outlook.com>
2018-07-21 10:41:04 -0500

    created user.js for db and added packages to encrypt pw

---
+++ models/user.js
@@ -0,0 +1,46 @@
+// Makes the User Model available for other files (will also create a table)
+
+module.exports = function(sequelize, Sequelize) {
+ 
+  var User = sequelize.define("User", {
+
+    firstname: {
+        type: Sequelize.STRING,
+        notEmpty: true
+    },
+
+    lastname: {
+        type: Sequelize.STRING,
+        notEmpty: true
+    },
+
+    username: {
+        type: Sequelize.TEXT
+    },
+
+    email: {
+        type: Sequelize.STRING,
+        validate: {
+            isEmail: true
+        }
+    },
+
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+
+    last_login: {
+        type: Sequelize.DATE
+    },
+
+    status: {
+        type: Sequelize.ENUM("active", "inactive"),
+        defaultValue: "active"
+    }
+
+  });
+  // Syncs with DB
+  User.sync();
+  return User;
+}
