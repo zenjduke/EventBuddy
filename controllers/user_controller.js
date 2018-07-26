341564f3daf966192a34a2b91928c8d1741f06e4
ruben-r <ruben.ruiz@outlook.com>
2018-07-25 20:32:33 -0500

    blocking out controller

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,16 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var burger = require('../models/burger.js');
+// var burger = require('../models/burger.js');
 
-
-
-
-router.get("/", function(req, res) {
-    burger.selectAll(function(data) {
-      var hbsObject = {
-        burgers: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
-    });
-  });
+//Controller not in use
